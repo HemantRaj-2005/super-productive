59788ff0b7f8434ae5b6488fd8c175cfef01c31d
Hemant Raj <coder.hemantraj05@gmail.com>
2024-12-13 18:10:14 +0530

    restoring codes from where error started, version before downgrading

--- lib/checkIfUserCompletedOnboarding.ts
+++ lib/checkIfUserCompletedOnboarding.ts
@@ -1,15 +1,16 @@
-import { redirect } from "next/navigation"
-import { getAuthSession } from "./auth"
+import { redirect } from "next/navigation";
+import { getAuthSession } from "./auth";
 
+export const checkIfUserCompletedOnboarding = async (currentPath: string) => {
+  const session = await getAuthSession();
+  if (!session) redirect("/");
 
+  if (session.user.completedOnboarding && currentPath === "/onboarding")
+    redirect("/dashboard");
 
-export const checkIfUserCompletedOnboarding = async(currentPath: string) => {
-    const session = await getAuthSession()
-    if(!session) redirect("/")
-    if(session.user.completedOnboarding && currentPath==="/onboarding") redirect("/dashboard")
-    if(!session.user.completedOnboarding && currentPath !== "/onboarding"){
-        redirect('/onboarding?error=not-completed-onboarding')
-    }
+  if (!session.user.completedOnboarding && currentPath !== "/onboarding") {
+    redirect("/onboarding?error=not-completed-onboarding");
+  }
 
-    return session;
-}
+  return session;
+};
